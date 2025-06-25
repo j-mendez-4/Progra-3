272008e83160e5e8b8886e24d545660ee90775a0
JohannesMéndez <j.mendez.4@estudiantec.cr>
2025-06-25 14:09:59 -0600

    Menú Principal + Elegir Juego

---
+++ main.py
@@ -0,0 +1,146 @@
+'''
+Progra 3 de Taller
+
+Consiste de dos juegos; un juego de cartas estilo "21" o "Ron",
+y otro que imita al juego cásico de "Fox and Houds" o "El zorro y los Sabuesos"
+'''
+
+import random
+import time
+from colorama import Fore, init
+init(autoreset=True)
+
+# ----------------- FUNCIONES AUXILIARES -----------------
+
+def es_numerico(string):
+    '''
+    Verfifica si un string contiene solo caracteres numericos
+
+    E: Un string
+    S: Un booleano que indica si el string es numerico
+    R: string debe ser un string, de lo contrario retorna False.
+    '''
+
+    if type(string) != str:
+        return False
+    
+    for elem in range(len(string)):
+
+        if string[elem] != "0" and string[elem] != "1" and string[elem] != "2" and \
+            string[elem] != "3" and string[elem] != "4" and string[elem] != "5" and \
+            string[elem] != "6" and string[elem] != "7" and string[elem] != "8" and \
+            string[elem] != "9":
+            return False
+        
+    return True
+
+
+# ----------------- FUNCIONES DEL JUEGO -----------------
+
+def menu_principal():
+    '''
+    Muestra el menú principal del juego y permite al usuario elegir una opción
+
+    E: Un numero del 1 al 4
+    S: Llama a la función encargada de la opción elegida por el usuario.
+    R: Se debe ingresar un número del 1 al 4.
+    '''
+
+    print(r'''
+
+##############################################################################
+#                                                                            #
+#                                                                            #
+#      __  __              __    ____       _            _             _     #
+#     |  \/  | ___ _ __  _/_/_  |  _ \ _ __(_)_ __   ___(_)_ __   __ _| |    #
+#     | |\/| |/ _ \ '_ \| | | | | |_) | '__| | '_ \ / __| | '_ \ / _` | |    #
+#     | |  | |  __/ | | | |_| | |  __/| |  | | | | | (__| | |_) | (_| | |    #
+#     |_|  |_|\___|_| |_|\__,_| |_|   |_|  |_|_| |_|\___|_| .__/ \__,_|_|    #
+#                                                         |_|                #
+#                                                                            #
+#                                                                            #
+##############################################################################
+
+          ''')
+    time.sleep(1)
+
+    print("1. Jugar") # -> Jugar
+    time.sleep(1)
+    print("2. Lore") # -> Lore
+    time.sleep(1)
+    print("3. Cómo jugar") # -> Instrucciones
+    time.sleep(1)
+    print("4. Créditos") # -> EasterEgg
+    time.sleep(1)
+    print("q. Salir") # -> Salir
+    time.sleep(1)
+
+    opción = input(f"\nElige una {Fore.YELLOW}opción:{Fore.RESET}")
+
+    if not es_numerico(opción) or opción != "q" or opción != "Q":
+        print("\nIgresa una opción valida! \n")
+        time.sleep(3)
+        return menu_principal()
+    
+    elif opción == "1":
+        return elegir_juego()
+    
+    elif opción == "2":
+        return ""
+    
+    elif opción == "3":
+        return ""
+    
+    elif opción == "4":
+        return ""
+    
+    elif opción == "q" or opción == "Q":
+        return ""
+    
+    else:
+        print(f"\nEsa no es una opción valida! \n")
+        time.sleep(3)
+        return menu_principal()
+    
+
+
+def elegir_juego():
+
+    print("Elige uno de los juegos para comenzar la partida:")
+    time.slep(1)
+    
+    print("1. La encuadrillada (Fox and Hounds)")
+    time.slep(1)
+
+    print("2. La Disputa (21 o Ron)")
+    time.slep(1)
+
+    print("q. Atrás")
+    time.slep(1)
+
+    opción = input(f"\nElige una {Fore.YELLOW}opción:{Fore.RESET}")
+
+    if not es_numerico(opción) or opción != "q" or opción != "Q":
+        print("\nIgresa una opción valida! \n")
+        time.sleep(3)
+        return menu_principal()
+    
+    elif opción == "1":
+        return "JUEGO 1"
+    
+    elif opción == "2":
+        return "JUEGO 2"
+    
+    elif opción == "q" or opción == "Q":
+        return menu_principal()
+    
+    else:
+        print(f"\nEsa no es una opción valida! \n")
+        time.sleep(3)
+        return menu_principal()
+
+
+# ---------------------- EXPERIMENTAL ----------------------
+
+res = menu_principal()
+print(res)
